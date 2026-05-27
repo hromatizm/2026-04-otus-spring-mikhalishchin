829637e688963d91c56634eb799b7addfef18df6
Михалищин Дмитрий Викторович <dmitriy.v.mikhalischin@gazprombank.ru>
2026-05-28 00:08:37 +0300

    Добавлен интеграционный тест ДЗ_2

--- hw02/src/test/java/ru/otus/hw/DaoIntegrationTest.java
+++ hw02/src/test/java/ru/otus/hw/DaoIntegrationTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.Mockito.mock;
@@ -27,10 +31,51 @@ public class DaoIntegrationTest {
 
         var dao = new CsvQuestionDao(fileNameProvider);
 
+        var expectedQuestions = List.of(
+                new Question(
+                        "Question_1?",
+                        List.of(
+                                new Answer("Answer_1_1", true),
+                                new Answer("Answer_1_2", false),
+                                new Answer("Answer_1_3", false)
+                        )),
+                new Question(
+                        "Question_2?",
+                        List.of(
+                                new Answer("Answer_2_1", false),
+                                new Answer("Answer_2_2", true),
+                                new Answer("Answer_2_3", false),
+                                new Answer("Answer_2_4", false)
+                        )),
+                new Question(
+                        "Question_3?",
+                        List.of(
+                                new Answer("Answer_3_1", true),
+                                new Answer("Answer_3_2", false),
+                                new Answer("Answer_3_3", false)
+                        )),
+                new Question(
+                        "Question_4?",
+                        List.of(
+                                new Answer("Answer_4_1", false),
+                                new Answer("Answer_4_2", true),
+                                new Answer("Answer_4_3", false),
+                                new Answer("Answer_4_4", false)
+                        )),
+                new Question(
+                        "Question_5?",
+                        List.of(
+                                new Answer("Answer_5_1", true),
+                                new Answer("Answer_5_2", false),
+                                new Answer("Answer_5_3", false)
+                        ))
+        );
+
         // Act
-        dao.findAll();
+        var actualQuestions = dao.findAll();
 
         // Assert
-        assertThat(dao.findAll()).hasSize(5);
+        assertThat(actualQuestions).hasSize(5);
+        assertThat(actualQuestions).containsExactlyElementsOf(expectedQuestions);
     }
 }
